b7fc9d72473b4893cb45589e370de92b7bd1d595
laxninja <nicole.cruthirds@gmail.com>
2015-07-23 21:54:17 -0400

    code version 2
    version 2

--- address.py
+++ address.py
@@ -1,8 +1,12 @@
+
+
+
+
 def address():
 
 	print"hi i'm your address book."
 
-	print"""1-open address book
+	print"""	1-open address book
 	2-add entry
 	3-remove entry
 	4-store address book
@@ -12,32 +16,49 @@ def address():
 	8-search names
 	9-search names and emails
 	10-quit"""
-	print"please pick a number"
-	anwser = int(raw_input())
-	if anwser > 10:
-		print"pick a number less then 11"
 	
-	if anwser == 1:
+	print "you always have to pick the number 1 if you pick 2 and 4"
+	print"please pick a number"
+	answer = (raw_input())
+	if answer == "1" or answer == "2" or answer == "10" or answer == "4"or answer == "5":
+		answer = int(answer)
+	
+	if answer > 10:
+		print"pick a number less then 11"
+		address()
+	elif answer == 1:
+		global dog
+		global file
 		print".csv at the end of the file name"
-
 		print"please enter a file name"
 		file = raw_input()
-
+		dog = open(file,"a")
+		address()
+	elif answer == 2:
+	
 		print"please enter the name of the person"
 		name = raw_input()
-
 		print"please enter there email"
 		email = raw_input()
-
-		dog = open(file,"a")
-
-
 		dog.write(name)
 		dog.write("\n")
 		dog.write(email)
-
+		address()
 		print file
 
 		dog.close()
+		address()
+		
+	elif answer == 4:
+		print"please enter the name of the person"
+		name = raw_input()
+		address()
+	
+	elif answer == 5:
+		list = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+		list.sort()
+		print list
+	elif answer == 10:
+		print 'Good bye.'
 
 address()
